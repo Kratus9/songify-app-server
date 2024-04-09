00a052d5e23465bb70463b48798a070c61c5fa66
Mark <marcboluda@hotmail.com>
2024-04-09 18:04:44 +0200

    prefijos indexRoutes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,7 +1,12 @@
+const authRoutes = require("./authRoutes");
+const musicRoutes = require("./musicRoutes");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.json("Welcome to Songify");
 });
+
+router.use("/auth", authRoutes);
+router.use("/music", musicRoutes);
 
 module.exports = router;
